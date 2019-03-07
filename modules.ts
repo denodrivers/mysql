5e5ef7961822192e37a7fe01c1cf85134bad3505
manyuanrong <416828041@qq.com>
2019-03-07 17:47:25 +0800

    update doc

--- modules.ts
+++
@@ -1,3 +0,0 @@
-export { format as byteFormat } from "https://deno.land/x/bytes_formater/mod.ts";
-export { test, assert, runTests } from "https://deno.land/x/testing/mod.ts";
-export { decode, encode } from "https://deno.land/x/std/strings/strings.ts";
