ca3725de6d9528b50907245473db6d6993ec8bbb
Enok <416828041@qq.com>
2019-11-15 12:53:40 +0800

    Bump to deno v0.24,0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { decode, encode } from "https://deno.land/std@v0.23.0/strings/mod.ts";
+export { decode, encode } from "https://deno.land/std@v0.24.0/strings/mod.ts";
 export { format as byteFormat } from "https://deno.land/x/bytes_formater@1.1.0/mod.ts";
 export { replaceParams } from "https://deno.land/x/sql_builder@1.3.5/util.ts";
