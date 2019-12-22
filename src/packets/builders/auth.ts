0c433d7a4538e1e4c0c2922bf6996d1a1be7ebf9
magichim <songwon.bak@gmail.com>
2019-12-22 19:24:48 +0900

    Change directory name constantts -> constant

--- src/packets/builders/auth.ts
+++ src/packets/builders/auth.ts
@@ -1,8 +1,8 @@
 import { BufferWriter } from "../../buffer.ts";
-import ServerCapabilities from "../../consttants/capabilities.ts";
+import ServerCapabilities from "../../constant/capabilities";
 import { auth } from "../../auth/mysql_native_password.ts";
 import { HandshakeBody } from "../parsers/handshake.ts";
-import { Charset } from "../../consttants/charset.ts";
+import { Charset } from "../../constant/charset";
 
 /** @ignore */
 export function buildAuth(
--- src/packets/parsers/err.ts
+++ src/packets/parsers/err.ts
@@ -1,6 +1,6 @@
 import { BufferReader } from "../../buffer.ts";
 import { Connection } from "../../connection.ts";
-import ServerCapabilities from "../../consttants/capabilities.ts";
+import ServerCapabilities from "../../constant/capabilities";
 
 /** @ignore */
 export interface ErrorPacket {
