d9af8ac92bba183960f1161bf7874e853d908f36
t-yng <t.yng.greenmonkey@gmail.com>
2020-04-05 21:23:35 +0900

    fix failed fetch std/strings

--- src/auth/mysql_native_password.ts
+++ src/auth/mysql_native_password.ts
@@ -1,5 +1,5 @@
 import { encode } from "../../deps.ts";
-import { Hash } from "https://deno.land/x/checksum@1.1.0/mod.ts";
+import { Hash } from "https://deno.land/x/checksum@1.2.0/mod.ts";
 
 /** @ignore */
 export function auth(password: string, seed: Uint8Array): Uint8Array {
