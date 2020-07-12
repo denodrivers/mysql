denodrivers/mysql

commit 813b8464c95e32f4e638d7ec747197316c2e46ed

Update sql_builder to v1.6.0 (#63)
Update sql_builder to v1.6.0
FIx #62

deps.ts
@@ -11,5 +11,5 @@ export {
 export { format as byteFormat } from "https://deno.land/x/bytes_formater@1.2.0/mod.ts";
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
 export { sha256 } from "https://deno.land/x/sha256@v1.0.2/mod.ts";
-export { replaceParams } from "https://deno.land/x/sql_builder@1.5.0/util.ts";
+export { replaceParams } from "https://deno.land/x/sql_builder@v1.6.0/util.ts";
 export * as log from "https://deno.land/x/std@0.53.0/log/mod.ts";

test.ts
@@ -119,6 +119,25 @@ testWithClient(async function testQueryDecimal(client) {
   assertEquals(result, [{ decimal_column: value }]);
 });
 
+testWithClient(async function testQueryDatetime(client) {
+  await client.query(`DROP TABLE IF EXISTS test_datetime`);
+  await client.query(`CREATE TABLE test_datetime (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    datetime datetime(6) NOT NULL,
+    PRIMARY KEY (id)
+  ) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4`);
+  const datetime = new Date();
+  await client.execute(
+    `
+    INSERT INTO test_datetime (datetime)
+    VALUES (?)`,
+    [datetime],
+  );
+
+  const [row] = await client.query("SELECT datetime FROM test_datetime");
+  assertEquals(row.datetime.toISOString(), datetime.toISOString()); // See https://github.com/denoland/deno/issues/6643
+});
+
 testWithClient(async function testDelete(client) {
   let result = await client.execute(`delete from users where ?? = ?`, [
     "id",

commit cf606197c6985cb9857c09cc8f1d83ac21e6cc7b

Fix CI failure introduced by #63

deps.ts
@@ -4,10 +4,6 @@ export {
   delay,
 } from "https://deno.land/std@0.53.0/async/mod.ts";
 export { decode, encode } from "https://deno.land/std@0.53.0/encoding/utf8.ts";
-export {
-  assertEquals,
-  assertThrowsAsync,
-} from "https://deno.land/std@0.53.0/testing/asserts.ts";
 export { format as byteFormat } from "https://deno.land/x/bytes_formater@1.2.0/mod.ts";
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
 export { sha256 } from "https://deno.land/x/sha256@v1.0.2/mod.ts";

test.deps.ts
@@ -1,0 +1,5 @@
+export {
+  assertEquals,
+  assertThrowsAsync,
+} from "https://deno.land/std@0.53.0/testing/asserts.ts";
+export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";

test.ts
@@ -1,7 +1,8 @@
 import {
   assertEquals,
   assertThrowsAsync,
-} from "./deps.ts";
+  semver,
+} from "./test.deps.ts";
 import { WriteError } from "./src/constant/errors.ts";
 import { createTestDB, testWithClient } from "./test.util.ts";
 
@@ -120,22 +121,28 @@ testWithClient(async function testQueryDecimal(client) {
 });
 
 testWithClient(async function testQueryDatetime(client) {
-  await client.query(`DROP TABLE IF EXISTS test_datetime`);
-  await client.query(`CREATE TABLE test_datetime (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    datetime datetime(6) NOT NULL,
-    PRIMARY KEY (id)
-  ) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4`);
-  const datetime = new Date();
-  await client.execute(
-    `
-    INSERT INTO test_datetime (datetime)
-    VALUES (?)`,
-    [datetime],
-  );
+  await client.useConnection(async (connection) => {
+    if (semver.lt(connection.serverVersion, "5.6.0")) {
+      return;
+    }
 
-  const [row] = await client.query("SELECT datetime FROM test_datetime");
-  assertEquals(row.datetime.toISOString(), datetime.toISOString()); // See https://github.com/denoland/deno/issues/6643
+    await client.query(`DROP TABLE IF EXISTS test_datetime`);
+    await client.query(`CREATE TABLE test_datetime (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      datetime datetime(6) NOT NULL,
+      PRIMARY KEY (id)
+    ) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4`);
+    const datetime = new Date();
+    await client.execute(
+      `
+      INSERT INTO test_datetime (datetime)
+      VALUES (?)`,
+      [datetime],
+    );
+
+    const [row] = await client.query("SELECT datetime FROM test_datetime");
+    assertEquals(row.datetime.toISOString(), datetime.toISOString()); // See https://github.com/denoland/deno/issues/6643
+  });
 });
 
 testWithClient(async function testDelete(client) {
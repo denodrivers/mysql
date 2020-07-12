cf606197c6985cb9857c09cc8f1d83ac21e6cc7b
uki00a <uki00a@gmail.com>
2020-07-12 10:41:29 +0900

    Fix CI failure introduced by #63

--- deps.ts
+++ deps.ts
@@ -4,10 +4,6 @@ export {
   delay,
 } from "https://deno.land/std@0.53.0/async/mod.ts";
 export { decode, encode } from "https://deno.land/std@0.53.0/encoding/utf8.ts";
-export {
-  assertEquals,
-  assertThrowsAsync,
-} from "https://deno.land/std@0.53.0/testing/asserts.ts";
 export { format as byteFormat } from "https://deno.land/x/bytes_formater@1.2.0/mod.ts";
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
 export { sha256 } from "https://deno.land/x/sha256@v1.0.2/mod.ts";
---
+++ test.deps.ts
@@ -0,0 +1,5 @@
+export {
+  assertEquals,
+  assertThrowsAsync,
+} from "https://deno.land/std@0.53.0/testing/asserts.ts";
+export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
