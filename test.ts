denodrivers/mysql

commit 813b8464c95e32f4e638d7ec747197316c2e46ed

Update sql_builder to v1.6.0 (#63)
Update sql_builder to v1.6.0
FIx #62

test.ts
@@ -119,6 +119,25 @@ testWithClient(async function testQueryDecimal(client) {
   assertEquals(result, [{ decimal_column: value }]);
 });
 
+testWithClient(async function testQueryDatetime(client) {
+  await client.query(`DROP TABLE IF EXISTS test_datetime`);
+  await client.query(`CREATE TABLE test_datetime (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    datetime datetime(6) NOT NULL,
+    PRIMARY KEY (id)
+  ) ENGINE=MEMORY DEFAULT CHARSET=utf8mb4`);
+  const datetime = new Date();
+  await client.execute(
+    `
+    INSERT INTO test_datetime (datetime)
+    VALUES (?)`,
+    [datetime],
+  );
+
+  const [row] = await client.query("SELECT datetime FROM test_datetime");
+  assertEquals(row.datetime.toISOString(), datetime.toISOString()); // See https://github.com/denoland/deno/issues/6643
+});
+
 testWithClient(async function testDelete(client) {
   let result = await client.execute(`delete from users where ?? = ?`, [
     "id",

deps.ts
@@ -11,5 +11,5 @@ export {
 export { format as byteFormat } from "https://deno.land/x/bytes_formater@1.2.0/mod.ts";
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
 export { sha256 } from "https://deno.land/x/sha256@v1.0.2/mod.ts";
-export { replaceParams } from "https://deno.land/x/sql_builder@1.5.0/util.ts";
+export { replaceParams } from "https://deno.land/x/sql_builder@v1.6.0/util.ts";
 export * as log from "https://deno.land/x/std@0.53.0/log/mod.ts";

commit 9a0a1831c2fd3341938c2bad69aad04d6b6f2158

Fix CI failure introduced by #63 (part2)

test.ts
@@ -4,7 +4,7 @@ import {
   semver,
 } from "./test.deps.ts";
 import { WriteError } from "./src/constant/errors.ts";
-import { createTestDB, testWithClient } from "./test.util.ts";
+import { createTestDB, testWithClient, isMariaDB } from "./test.util.ts";
 
 testWithClient(async function testCreateDb(client) {
   await client.query(`CREATE DATABASE IF NOT EXISTS enok`);
@@ -122,7 +122,7 @@ testWithClient(async function testQueryDecimal(client) {
 
 testWithClient(async function testQueryDatetime(client) {
   await client.useConnection(async (connection) => {
-    if (semver.lt(connection.serverVersion, "5.6.0")) {
+    if (isMariaDB(connection) || semver.lt(connection.serverVersion, "5.6.0")) {
       return;
     }
 

test.util.ts
@@ -1,4 +1,4 @@
-import { Client } from "./mod.ts";
+import { Client, Connection } from "./mod.ts";
 
 const { DB_PORT, DB_NAME, DB_PASSWORD, DB_USER, DB_HOST } = Deno.env.toObject();
 const port = DB_PORT ? parseInt(DB_PORT) : 3306;
@@ -43,4 +43,8 @@ export async function createTestDB() {
   });
   await client.execute(`CREATE DATABASE IF NOT EXISTS ${db}`);
   await client.close();
+}
+
+export function isMariaDB(connection: Connection): boolean {
+  return connection.serverVersion.includes("MariaDB");
 }