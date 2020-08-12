c7770638ebe593d91e50923db4cfba321609bc3f
George Xie <georgexie@George.local>
2020-08-12 10:47:08 +0800

    fix: switch to god_crypto lib, because it's a tiny deno lib

--- src/auth_plugin/crypt.ts
+++ src/auth_plugin/crypt.ts
@@ -1,15 +1,7 @@
-import "../lib/forge.js";
-const forge = (self as any).forge;
-const { publicKeyFromPem } = forge.pki;
+import { RSA } from "https://deno.land/x/god_crypto@v0.2.0/mod.ts";
 function encryptWithPublicKey(key: string, data: Uint8Array): Uint8Array {
-  const publicKey = publicKeyFromPem(key);
-  const result = Array.prototype.map.call(data, (i) => String.fromCharCode(i))
-    .join("");
-  const cipher = publicKey.encrypt(result, "RSA-OAEP");
-  const encryptedData: Uint8Array = cipher.split("").map((item: string) =>
-    item.charCodeAt(0)
-  );
-  return encryptedData;
+  const publicKey = RSA.parseKey(key);
+  return RSA.encrypt(data, publicKey);
 }
 
 export { encryptWithPublicKey };
