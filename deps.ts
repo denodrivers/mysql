ff69b79f2e85d83d5e795d8cde24258baf217542
Chris Couzens <ccouzens@gmail.com>
2020-09-24 21:33:11 +0100

    Use deno std dependencies without the /x/ in the URL
    This makes it consistent with other deno std imports like async/mod.ts.

--- deps.ts
+++ deps.ts
@@ -8,4 +8,4 @@ export { format as byteFormat } from "https://deno.land/x/bytes_formater@v1.3.0/
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
 export { sha256 } from "https://deno.land/x/sha256@v1.0.2/mod.ts";
 export { replaceParams } from "https://deno.land/x/sql_builder@v1.6.0/util.ts";
-export * as log from "https://deno.land/x/std@0.63.0/log/mod.ts";
+export * as log from "https://deno.land/std@0.63.0/log/mod.ts";
