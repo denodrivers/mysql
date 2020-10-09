f83b35adf488372ac7548172d083185afde788d5
u-ways <ue95.career@gmail.com>
2020-10-09 03:21:19 +0100

    Fix formatting issues - uways

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
-export type {Deferred} from "https://deno.land/std@0.63.0/async/mod.ts";
-export {deferred, delay} from "https://deno.land/std@0.63.0/async/mod.ts";
-export {decode, encode} from "https://deno.land/std@0.63.0/encoding/utf8.ts";
-export {format as byteFormat} from "https://deno.land/x/bytes_formater@v1.3.0/mod.ts";
-export {Hash} from "https://deno.land/x/checksum@1.4.0/mod.ts";
-export {sha256} from "https://deno.land/x/sha256@v1.0.2/mod.ts";
-export {replaceParams} from "https://deno.land/x/sql_builder@v1.6.0/util.ts";
+export type { Deferred } from "https://deno.land/std@0.63.0/async/mod.ts";
+export { deferred, delay } from "https://deno.land/std@0.63.0/async/mod.ts";
+export { decode, encode } from "https://deno.land/std@0.63.0/encoding/utf8.ts";
+export { format as byteFormat } from "https://deno.land/x/bytes_formater@v1.3.0/mod.ts";
+export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
+export { sha256 } from "https://deno.land/x/sha256@v1.0.2/mod.ts";
+export { replaceParams } from "https://deno.land/x/sql_builder@v1.6.0/util.ts";
 export * as log from "https://deno.land/std@0.63.0/log/mod.ts";
