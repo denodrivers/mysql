b4b82ec404348cddc3f12ce9980525e92dc08f02
U-ways <ue95.career@gmail.com>
2020-10-10 02:12:54 +0100

    Add import type for imports not used as a value (#77)

--- deps.ts
+++ deps.ts
@@ -1,8 +1,5 @@
-export {
-  deferred,
-  Deferred,
-  delay,
-} from "https://deno.land/std@0.63.0/async/mod.ts";
+export type { Deferred } from "https://deno.land/std@0.63.0/async/mod.ts";
+export { deferred, delay } from "https://deno.land/std@0.63.0/async/mod.ts";
 export { decode, encode } from "https://deno.land/std@0.63.0/encoding/utf8.ts";
 export { format as byteFormat } from "https://deno.land/x/bytes_formater@v1.3.0/mod.ts";
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
