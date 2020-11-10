286b8e2bc7482af2f07003117c784ba41393d337
alejandroq <quesadaaba@gmail.com>
2020-11-09 23:12:14 -0500

    updated byteFormat dependency

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export type { Deferred } from "https://deno.land/std@0.74.0/async/mod.ts";
 export { deferred, delay } from "https://deno.land/std@0.74.0/async/mod.ts";
 export { decode, encode } from "https://deno.land/std@0.74.0/encoding/utf8.ts";
-export { format as byteFormat } from "https://deno.land/x/bytes_formater@v1.3.0/mod.ts";
+export { format as byteFormat } from "https://deno.land/x/bytes_formater@v1.4.0/mod.ts";
 export { Hash } from "https://deno.land/x/checksum@1.4.0/mod.ts";
 export { sha256 } from "https://deno.land/x/sha256@v1.0.2/mod.ts";
 export { replaceParams } from "https://deno.land/x/sql_builder@v1.7.0/util.ts";
