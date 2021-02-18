d9a3cd6757e15b5fdd3eb0980d27c2b9e5ffa424
lideming <me@yuuza.net>
2021-02-19 05:05:38 +0800

    Add configLogger()

--- mod.ts
+++ mod.ts
@@ -1,3 +1,8 @@
 export type { ClientConfig } from "./src/client.ts";
 export { Client } from "./src/client.ts";
 export { Connection } from "./src/connection.ts";
+
+export type { LoggerConfig } from "./src/logger.ts";
+export { configLogger } from "./src/logger.ts";
+
+export { log } from "./deps.ts";
