2ad3fcd9f488ac95e1e3a4ed80c6eedcd76181f8
lideming <me@yuuza.net>
2021-05-03 03:49:38 +0800

    Add test

--- test.ts
+++ test.ts
@@ -287,6 +287,25 @@ testWithClient(async function testLargeQueryAndResponse(client) {
   );
 });
 
+testWithClient(async function testExecuteIterator(client) {
+  await client.useConnection(async conn => {
+    await conn.execute(`DROP TABLE IF EXISTS numbers`);
+    await conn.execute(`CREATE TABLE numbers (num INT NOT NULL)`);
+    await conn.execute(`INSERT INTO numbers (num) VALUES ${
+      new Array(64).fill(0).map((v, idx) =>
+        `(${idx})`
+      ).join(',')
+    }`);
+    const r = await conn.execute(`SELECT num FROM numbers`, [], true);
+    let count = 0;
+    for await (const row of r.iterator) {
+      assertEquals(row.num, count);
+      count++;
+    }
+    assertEquals(count, 64);
+  });
+});
+
 registerTests();
 
 Deno.test("configLogger()", async () => {
