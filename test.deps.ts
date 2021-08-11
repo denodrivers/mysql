8e95e52478c1576086fa5dd6a6462e8ca54796d8
lideming <me@yuuza.net>
2021-08-12 01:33:13 +0800

    Bump semver to v1.4.0

--- test.deps.ts
+++ test.deps.ts
@@ -2,5 +2,5 @@ export {
   assertEquals,
   assertThrowsAsync,
 } from "https://deno.land/std@0.104.0/testing/asserts.ts";
-export * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+export * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
 export { parse } from "https://deno.land/std@0.104.0/flags/mod.ts";
