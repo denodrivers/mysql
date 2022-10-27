f816775360101f9df100f311f7b3611e3a3f98f3
Yoshiya Hinosawa <stibium121@gmail.com>
2022-10-27 20:18:28 +0900

    fix: update god_crypto

--- src/auth_plugin/crypt.ts
+++ src/auth_plugin/crypt.ts
@@ -1,4 +1,4 @@
-import { RSA } from "https://deno.land/x/god_crypto@v0.2.0/mod.ts";
+import { RSA } from "https://deno.land/x/god_crypto@v1.4.10/mod.ts";
 function encryptWithPublicKey(key: string, data: Uint8Array): Uint8Array {
   const publicKey = RSA.parseKey(key);
   return RSA.encrypt(data, publicKey);
