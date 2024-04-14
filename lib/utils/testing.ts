62225bd29a4bd25dd1f7533e9fea300b8d4b52ac
Halvard Mørstad <halvard@simplyundoable.com>
2024-04-14 20:16:39 +0200

    fmt

--- lib/utils/testing.ts
+++ lib/utils/testing.ts
@@ -3,4 +3,5 @@ import { resolve } from "@std/path";
 export const DIR_TMP_TEST = resolve("tmp_test");
 
 //socket "/var/run/mysqld/mysqld.sock";
-export const URL_TEST_CONNECTION = Deno.env.get("DENO_MYSQL_CONNECTION_URL")||"mysql://root@0.0.0.0:3306/testdb"
+export const URL_TEST_CONNECTION = Deno.env.get("DENO_MYSQL_CONNECTION_URL") ||
+  "mysql://root@0.0.0.0:3306/testdb";
